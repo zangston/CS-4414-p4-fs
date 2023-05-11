5188abfa69b158a5b52f0e2ecf227fc4005970a4
zangston <winston.y.zhang@gmail.com>
2023-05-11 20:30:02 +0000

    Implement file reading in fsdump.c

--- exp1/fsdump.c
+++ exp1/fsdump.c
@@ -6,6 +6,31 @@
 
 int main(int argc, char *argv[])
 {
-    printf("test");
+    // Check if a file has been specified in execution command
+    if(argc < 2)
+    {
+        printf("Error: no filename supplied\n");
+        printf("Expected command: %s name\n", argv[0]);
+        return 1;
+    }
+
+    // Read filename from command line args
+    char *filename = argv[1];
+
+    // Attempt to open file
+    FILE *file = fopen(filename, "rb");
+
+    // Throw error if file not found
+    if(file == NULL)
+    {
+        printf("Error: File %s not found\n", argv[1]);
+        return 1;
+    }
+
+    
+
+    // Close .img file
+    fclose(file);
+
     return 0;
 }
